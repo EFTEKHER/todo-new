d11f294853a38afab71e1d0808d25f95ee667dee
EFTEKHER <eftekherali2000@gmail.com>
2024-04-14 15:35:16 +0600

    done

---
+++ todo-new/src/context/index.js
@@ -0,0 +1,3 @@
+
+export{TodoContext, TodoProvider,useTodo} from "./todoContext";;
+
---
+++ todo-new/src/context/todoContext.js
@@ -0,0 +1,27 @@
+import { createContext,useContext } from "react"
+
+
+export const TodoContext=createContext({
+    todos:[
+        {
+            id:1,
+            todo:'todo msg',
+            completed:false
+        }
+
+    ],
+
+    addTodo:(todo)=>{},
+    updatedTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+
+
+})
+
+export const useTodo=()=>{
+    
+   return useContext(TodoContext)
+}
+
+export const TodoProvider=TodoContext.Provider;
